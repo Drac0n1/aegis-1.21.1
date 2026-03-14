6975a6b07fc77232cde519260aa73b46c41925f6
Drakos <boxergamer3600@gmail.com>
2026-03-14 23:21:58 +0000

    Initial commit

---
+++ src/main/java/aegis/AegisClient.java
@@ -0,0 +1,10 @@
+package aegis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AegisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/aegis/AegisDataGenerator.java
@@ -0,0 +1,11 @@
+package aegis;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AegisDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
