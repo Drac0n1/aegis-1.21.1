b824591467772dfcf64b8d3f47e1b2ea852e584e
Drakos <boxergamer3600@gmail.com>
2026-03-15 15:46:26 +0000

    wip adding magma shield v2 pt2

--- src/main/java/aegis/item/ModItems.java
+++ src/main/java/aegis/item/ModItems.java
@@ -4,7 +4,6 @@ import aegis.Aegis;
 import aegis.item.custom.Magma_Shield_V2_Item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ShieldItem;
 import net.minecraft.registry.Registries;
@@ -13,13 +12,24 @@ import net.minecraft.util.Identifier;
 
 
 public class ModItems {
-    public static final Item MAGMA_SHIELD_V2 = registerItem("magma_shield_v2", new ShieldItem(new Item.Settings().maxDamage(336)));
+
+
+    public static final Item MAGMA_SHIELD_V2 = registerItem("magma_shield_v2", new Magma_Shield_V2_Item(new Item.Settings().maxDamage(336)));
+
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Aegis.MOD_ID, name), item);
     }
+
+
+
+
+
     public static void registerModItems () {
+
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(MAGMA_SHIELD_V2);
+
         });
     }
 }
--- src/main/java/aegis/item/custom/Magma_Shield_V2_Item.java
+++ src/main/java/aegis/item/custom/Magma_Shield_V2_Item.java
@@ -1,49 +1,19 @@
 package aegis.item.custom;
 
-
-import net.minecraft.block.DispenserBlock;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
-import net.minecraft.world.World;
+import net.minecraft.item.Equipment;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.ShieldItem;
 
 
-
-public class Magma_Shield_V2_Item  extends Item implements Equipment {
+public class Magma_Shield_V2_Item  extends ShieldItem implements Equipment {
     public Magma_Shield_V2_Item(Settings settings) {
         super(settings);
-        DispenserBlock.registerBehavior(this, ArmorItem.DISPENSER_BEHAVIOR);
     }
-
-    @Override
-    public UseAction getUseAction(ItemStack stack) {
-        return UseAction.BLOCK;
-    }
-
-    @Override
-    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
-        return 72000;
-    }
-
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-        user.setCurrentHand(hand);
-        return TypedActionResult.consume(itemStack);
-    }
-
     @Override
     public boolean canRepair(ItemStack stack, ItemStack ingredient) {
         return ingredient.isOf(Items.MAGMA_BLOCK) || super.canRepair(stack, ingredient);
     }
 
-    @Override
-    public EquipmentSlot getSlotType() {
-        return EquipmentSlot.OFFHAND;
-    }
 
 }
